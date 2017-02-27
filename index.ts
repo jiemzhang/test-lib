9674db3df520a3519f935423b9375d988e6b0a9a
jiemzhang <jj.mzhang@gmail.com>
2017-02-26 22:35:08 -0500

    move index.ts

--- index.ts
+++ index.ts
@@ -2,10 +2,10 @@ import {NgModule, ModuleWithProviders} from '@angular/core';
 
 import {CommonModule} from '@angular/common';
 
-import { AppModule } from './app/app.module';
-import { ChartComponent } from './app/chart/chart.component';
+import { AppModule } from './src/app/app.module';
+import { ChartComponent } from './src/app/chart/chart.component';
 
-export * from './app/chart/chart.component';
+export * from './src/app/chart/chart.component';
 
 @NgModule({
   imports: [
